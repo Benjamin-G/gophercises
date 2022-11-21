095c56ed8ac88e8a7366d10045f96c3362d7428a
benjamin-g <brgeyer49@gmail.com>
2022-11-21 15:57:55 -0800

    feat: go mod import and csv test

---
+++ exercises/two.go
@@ -0,0 +1,7 @@
+package exercises
+
+import "fmt"
+
+func Two() {
+	fmt.Println("Hello, world Two")
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"gophercises/exercises"
+)
+
+func main() {
+	fmt.Println("Hello, world")
+	exercises.One()
+	exercises.Two()
+}
