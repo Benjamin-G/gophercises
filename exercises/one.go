36ba60496988e72773a18d700820896afbaf888d
benjamin-g <brgeyer49@gmail.com>
2022-11-22 12:38:28 -0800

    feat: 1/2 complete with exercise

--- exercises/one.go
+++ exercises/one.go
@@ -1,19 +1,40 @@
 package exercises
 
 import (
+	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
+	"os"
+	"runtime"
 	"strings"
 )
 
 func One() {
-	in := `first_name,last_name,username
-"Rob","Pike",rob
-Ken,Thompson,ken`
+	fmt.Println(flag.Arg(0))
 
-	r := csv.NewReader(strings.NewReader(in))
+	filename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer")
+
+	fmt.Println(*filename)
+
+	flag.Usage = func() {
+		fmt.Printf("Usage of %s \n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	// Must parse the flag arguments before reference
+	fmt.Println(*filename)
+
+	f, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(f)
 
 	for {
 		record, err := r.Read()
@@ -26,4 +47,31 @@ Ken,Thompson,ken`
 
 		fmt.Println(record)
 	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Simple Shell")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+
+		fmt.Println(strings.EqualFold("hi", text))
+
+		if runtime.GOOS == "windows" {
+			text = strings.Replace(text, "\r\n", "", -1)
+		} else {
+			text = strings.Replace(text, "\n", "", -1)
+		}
+
+		if strings.Compare("hi", text) == 0 {
+			fmt.Println("hello, Yourself")
+		}
+
+		if strings.Compare("exit", text) == 0 {
+			break
+		}
+	}
+	fmt.Println("Bye!")
 }
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"gophercises/exercises"
 )
 
 func main() {
-	fmt.Println("Hello, world")
 	exercises.One()
-	exercises.Two()
+	// exercises.Two()
 }
