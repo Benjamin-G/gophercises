19f80d614d58b510bdd78f1fc424173642675506
benjamin-g <brgeyer49@gmail.com>
2022-11-30 10:43:28 -0800

    feat: Exe 2 URL Shortern

--- exercises/two.go
+++ exercises/two.go
@@ -1,12 +1,47 @@
 package exercises
 
 import (
+	"flag"
 	"fmt"
 	"gophercises/urlshort"
+	"log"
 	"net/http"
+	"os"
 )
 
+var fileName *string
+
+func init() {
+	fileName = flag.String("yml", "exe2.yml", "a yml file of mapped paths to url")
+
+	flag.Usage = func() {
+		fmt.Printf("Usage of %s \n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+}
+
+func readDataString(fileName string) ([]byte, error) {
+	f, err := os.ReadFile(fileName)
+
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+
+	if err != nil {
+		return []byte(yaml), err
+	}
+
+	return f, nil
+}
+
 func Two() {
+	// filename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer")
+	fmt.Println(fileName)
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -16,18 +51,16 @@ func Two() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yml, err := readDataString(*fileName)
 	if err != nil {
-		panic(err)
+		log.Println("Error readDataString:", err)
 	}
+
+	yamlHandler, err := urlshort.YAMLHandler(yml, mapHandler)
+	if err != nil {
+		log.Println("Error YAMLHandler:", err)
+	}
+
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
 }
