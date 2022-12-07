23ecf7b06ff7c95a6e80a5d6e67e9c01141559b1
benjamin-g <brgeyer49@gmail.com>
2022-12-07 15:35:36 -0800

    feat: test performance

--- tests/generators_test.go
+++ tests/generators_test.go
@@ -2,14 +2,72 @@ package tests
 
 import "testing"
 
-// func BenchmarkGeneric(b *testing.B) {
-// 	done := make(chan interface{})
-// 	defer close(done)
+// You can see that the type-specific stages are twice as fast, but only marginally faster in magnitude. Generally, the limiting factor on your pipeline will either be your generator, or one of the stages that is computationally intensive. If the generator isn’t creating a stream from memory as with the repeat and repeatFn generators, you’ll probably be I/O bound. Reading from disk or the network will likely eclipse the meager performance overhead shown here.
+func BenchmarkGeneric(b *testing.B) {
+	repeat := func(
+		done <-chan interface{},
+		values ...interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
 
-// 	b.ResetTimer()
-// 	for range toString(done, take(done, repeat(done, "a"), b.N)) {
-// 	}
-// }
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	toString := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+	) <-chan string {
+		stringStream := make(chan string)
+		go func() {
+			defer close(stringStream)
+			for v := range valueStream {
+				select {
+				case <-done:
+					return
+				case stringStream <- v.(string):
+				}
+			}
+		}()
+		return stringStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	b.ResetTimer()
+	for range toString(done, take(done, repeat(done, "a"), b.N)) {
+	}
+}
 
 func BenchmarkTyped(b *testing.B) {
 	repeat := func(done <-chan interface{}, values ...string) <-chan string {
