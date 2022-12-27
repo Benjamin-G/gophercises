3b45eb7dd50dbccf418a184410fb592ce42e13a4
benjamin-g <brgeyer49@gmail.com>
2022-12-26 16:13:44 -0800

    feat: exercise 3 started

--- exercises/exercise-three.go
+++ exercises/exercise-three.go
@@ -1,7 +1,35 @@
 package exercises
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
 
 func Three() {
-	fmt.Println("Hello, world")
+	fmt.Println("Exercise Three Started:")
+	tmpl := template.Must(template.ParseFiles("./html/layout.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+	fmt.Println("Listening on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	exercises.OriellyContext()
+	exercises.Three()
 }
