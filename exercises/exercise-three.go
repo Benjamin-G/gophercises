95f86b9d30ad5133a71d82ad6bd75db29cbe3f43
benjamin-g <brgeyer49@gmail.com>
2022-12-30 11:44:23 -0800

    feat: parse json and query params

--- exercises/exercise-three.go
+++ exercises/exercise-three.go
@@ -4,108 +4,63 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
-type Todo struct {
-	Title string
-	Done  bool
+type PageObject struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
 }
 
-type TodoPageData struct {
-	PageTitle string
-	Todos     []Todo
+func unmarshalPageObject(data []byte) map[string]PageObject {
+	var z map[string]PageObject
+
+	if err := json.Unmarshal(data, &z); err != nil {
+		log.Fatal("Error when Unmarshaling json: ", err)
+		return nil
+	}
+
+	return z
 }
 
-type Data struct {
-	Intro struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"intro"`
-	NewYork struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"new-york"`
-	Debate struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"debate"`
-	SeanKelly struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"sean-kelly"`
-	MarkBates struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"mark-bates"`
-	Denver struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"denver"`
-	Home struct {
-		Title   string        `json:"title"`
-		Story   []string      `json:"story"`
-		Options []interface{} `json:"options"`
-	} `json:"home"`
-}
-
-func exampleHandler(w http.ResponseWriter, r *http.Request) {
-	content, err := ioutil.ReadFile("./gopher.json")
+func cyoa(w http.ResponseWriter, r *http.Request) {
+	content, err := os.ReadFile("./gopher.json")
 	if err != nil {
 		log.Fatal("Error when opening file: ", err)
 	}
 
-	var payload Data
-	err = json.Unmarshal(content, &payload)
-	if err != nil {
-		log.Fatal("Error during Unmarshal(): ", err)
+	pages := unmarshalPageObject(content)
+
+	query := strings.TrimSpace(strings.TrimPrefix(r.URL.Path, "/"))
+	log.Printf("query: %s\n", query)
+
+	page := pages["intro"]
+	for k := range pages {
+		if k == query {
+			page = pages[k]
+			break
+		}
 	}
 
-	// Let's print the unmarshalled data!
-	log.Printf("origin: %s\n", payload.Intro.Title)
-	log.Printf("origin: %s\n", payload.Intro.Story)
-
 	tmpl := template.Must(template.ParseFiles("./html/layout.html"))
-	// data := TodoPageData{
-	// 	PageTitle: "My TODO list",
-	// 	Todos: []Todo{
-	// 		{Title: "Task 1", Done: false},
-	// 		{Title: "Task 2", Done: true},
-	// 		{Title: "Task 3", Done: true},
-	// 	},
-	// }
-	tmpl.Execute(w, payload.Intro)
+	tmpl.Execute(w, page)
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Not Found", http.StatusNotFound)
 }
 
 func Three() {
 	fmt.Println("Exercise Three Started...")
 	fmt.Println("Listening on http://localhost:8080")
-	http.HandleFunc("/", exampleHandler)
+	http.HandleFunc("/", cyoa)
+	http.HandleFunc("/favicon.ico", faviconHandler)
 	http.ListenAndServe(":8080", nil)
 }
